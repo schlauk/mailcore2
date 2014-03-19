6f87d6056770e86ee4af47417d2f51cf9d41cecd
Karsten Schlautmann <github@karstens-email.de>
2014-03-19 21:40:42 +0100

    TMMFldrViewController added

---
+++ example/mac/macExample/macExample/TMMFldrViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMMFldrViewController.h
+//  macExample
+//
+//  Created by Karsten Schlautmann on 28.10.13.
+//  Copyright (c) 2013 MailCore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TMMFldrViewController : NSViewController
+
+@end
---
+++ example/mac/macExample/macExample/TMMFldrViewController.m
@@ -0,0 +1,32 @@
+//
+//  TMMFldrViewController.m
+//  macExample
+//
+//  Created by Karsten Schlautmann on 28.10.13.
+//  Copyright (c) 2013 MailCore. All rights reserved.
+//
+
+#import "TMMFldrViewController.h"
+
+@interface TMMFldrViewController ()
+
+@end
+
+@implementation TMMFldrViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (NSInteger) numberOfRowsInTableView:(NSTableView *)aTableView
+{
+    return 0;
+}
+
+@end
